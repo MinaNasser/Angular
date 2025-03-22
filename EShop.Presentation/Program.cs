66950c4ce6e2e7bb48290f318fe89ba50161341b
hebamsamy <hebasamy655@gmail.com>
2025-03-22 13:05:00 +0200

    Day 7

--- EShop.Presentation/Program.cs
+++ EShop.Presentation/Program.cs
@@ -1,29 +1,39 @@
 using EF_Core;
+using EF_Core.Models;
 using EShop.Manegers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-//DI Containers
+#region DI Containers
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<EShopContext>(i =>
 i.UseLazyLoadingProxies()
 .UseSqlServer(builder.Configuration.GetConnectionString("MyDB")));
-//builder.Services.AddScoped(typeof(EShopContext));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<EShopContext>();
+
 builder.Services.AddScoped(typeof(ProductManager));
-builder.Services.AddScoped(typeof(CategoryManager));
+builder.Services.AddScoped(typeof(CategoryManager)); 
+builder.Services.AddScoped(typeof(AccountManager));
+#endregion
 
 
 var app = builder.Build();
 
+#region Add / Use Middelware
 app.UseRouting();
 
 app.UseStaticFiles();//Force WWWRoot
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=index}");
+    pattern: "{controller=Home}/{action=index}"); 
+#endregion
 
 app.Run();
