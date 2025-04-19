e09ef9b1fe2039e7d8d0cb76ee751763d3f16f8c
MinaAl5al <minanasser82018@gmail.com>
2025-04-19 04:30:07 +0200

    ng g i Istore ng g class store

--- lec1/src/app/components/home/home.component.ts
+++ lec1/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Istore } from '../../Models/istore';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  myStore:Istore;
 
+  constructor(){  
+    this.myStore = {
+      name: "My Store",
+      imgUrl: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+      brand: ["Apple", "Samsung", "Huawei"]
+    }
+
+  }
+  print(){
+    console.log(this.myStore);
+  }
 }
