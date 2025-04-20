6fca3fda0d157a1753c8e667bb6f247524159dca
MinaAl5al <minanasser82018@gmail.com>
2025-04-21 00:01:04 +0200

    Product add cards

--- ECommerceApp/src/app/Components/product/product.component.ts
+++ ECommerceApp/src/app/Components/product/product.component.ts
@@ -17,13 +17,13 @@ export class ProductComponent {
   constructor() {
     this.products = [
       { id: 1, name: 'Laptop', price: 100, quantity: 10, imgUrl: 'https://picsum.photos/200/300', categoryId: 1 },
-      { id: 2, name: 'Laptop Dell', price: 100, quantity: 10, imgUrl: 'https://picsum.photos/200/300', categoryId: 1 },
-      { id: 3, name: 'Phone', price: 200, quantity: 20, imgUrl: 'https://picsum.photos/200/300', categoryId: 2 },
-      { id: 4, name: 'Tablet', price: 300, quantity: 30, imgUrl: 'https://picsum.photos/200/300', categoryId: 2 },
-      { id: 5, name: 'Monitor', price: 400, quantity: 40, imgUrl: 'https://picsum.photos/200/300', categoryId: 4 },
-      { id: 6, name: 'Mouse', price: 500, quantity: 50, imgUrl: 'https://picsum.photos/200/300', categoryId: 4 },
-      { id: 7, name: 'Keyboard', price: 600, quantity: 60, imgUrl: 'https://picsum.photos/200/300', categoryId: 4 },
-      { id: 8, name: 'Headphones', price: 700, quantity: 70, imgUrl: 'https://picsum.photos/200/300', categoryId: 4 },
+      { id: 2, name: 'Laptop Dell', price: 100, quantity: 10, imgUrl: 'https://picsum.photos/200/301', categoryId: 1 },
+      { id: 3, name: 'Phone', price: 200, quantity: 20, imgUrl: 'https://picsum.photos/200/302', categoryId: 2 },
+      { id: 4, name: 'Tablet', price: 300, quantity: 30, imgUrl: 'https://picsum.photos/200/303', categoryId: 2 },
+      { id: 5, name: 'Monitor', price: 400, quantity: 40, imgUrl: 'https://picsum.photos/200/304', categoryId: 4 },
+      { id: 6, name: 'Mouse', price: 500, quantity: 50, imgUrl: 'https://picsum.photos/200/305', categoryId: 4 },
+      { id: 7, name: 'Keyboard', price: 600, quantity: 60, imgUrl: 'https://picsum.photos/200/306', categoryId: 4 },
+      { id: 8, name: 'Headphones', price: 700, quantity: 70, imgUrl: 'https://picsum.photos/200/307', categoryId: 4 },
     ];
   }
 }
