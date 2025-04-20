6ced424a806c10d8a48c68472492fefe30320267
MinaAl5al <minanasser82018@gmail.com>
2025-04-21 01:44:12 +0200

    Filter

--- ECommerceApp/src/app/Components/product/product.component.ts
+++ ECommerceApp/src/app/Components/product/product.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { iProduct } from '../../Models/iproduct';
 import { CommonModule } from '@angular/common';
+import { iCategory } from '../../Models/icategory';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
     
   ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+
   products: iProduct[];
+  Categories: iCategory[] ;
   totalOrderPrice: number = 0;
+  selectedCategory: number = 0;
   constructor() {
     this.products = [
       { id: 1, name: 'Laptop', price: 100, quantity: 10, imgUrl: 'https://picsum.photos/200/300', categoryId: 1 },
@@ -26,10 +32,30 @@ export class ProductComponent {
       { id: 7, name: 'Keyboard', price: 600, quantity: 60, imgUrl: 'https://picsum.photos/200/306', categoryId: 4 },
       { id: 8, name: 'Headphones', price: 700, quantity: 70, imgUrl: 'https://picsum.photos/200/307', categoryId: 4 },
     ];
+    this.Categories = [
+      { id: 1, name: 'Electronics' },
+      { id: 2, name: 'Clothing' },
+      { id: 3, name: 'Home' },
+      { id: 4, name: 'Sports' },
+      { id: 5, name: 'Beauty' },
+      { id: 6, name: 'Toys' },
+      { id: 7, name: 'Books' },
+      { id: 8, name: 'Furniture' },
+      { id: 9, name: 'Jewelry' },
+      { id: 10, name: 'Shoes' },
+    ];
   }
   Buy(count: string, price: number) {
     // Convert the count to a number and check if it's a valid number
     const countNumber = parseInt(count, 10);
     this.totalOrderPrice += countNumber * price;
+  }
+  Filter() {
+    if (this.selectedCategory != 0) {
+      return this.products.filter((product) => product.categoryId == this.selectedCategory);
+    } else if( this.selectedCategory == 0) {
+      return this.products;
+    }
+    return this.products.filter((product) => product.categoryId == this.selectedCategory);
   }
 }
