6ae5e85913316fd035ecdaa590c909f39e1fa7bf
MinaAl5al <minanasser82018@gmail.com>
2025-04-24 09:33:13 +0200

    Account

---
+++ Day2&3/angular-app-master/src/app/Models/iuser-register.ts
@@ -0,0 +1,4 @@
+export interface IUserRegister {
+    phoneNumber: string;
+    ConfirmPassword: string;
+}
--- Day2&3/angular-app-master/src/app/app/app.route.ts
+++ Day2&3/angular-app-master/src/app/app/app.route.ts
@@ -4,6 +4,9 @@ import { todolistComponent } from "./Components/todolist/todolist.component";
 import { NotfoundComponent } from "./Components/notfound/notfound.component";
 
 export let AppRouts:Routes=[
+    {path :"", redirectTo:"products" ,pathMatch:"full"},
+    
+    {path:"product",component:ProductListComponent},
     {path:"products",component:ProductListComponent},
     {path:"todolist",component:todolistComponent},
     {path:"**" , component:NotfoundComponent}
