8c53aae30aa6838b81fdabcd28d1945b2b02341a
MinaAl5al <minanasser82018@gmail.com>
2025-04-26 10:54:15 +0300

    header

--- recipes-app/src/app/app.routes.ts
+++ recipes-app/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
 
-export const routes: Routes = [];
 
+export const routes: Routes = [
+    { path: '', component: RecipesListComponent },
+    { path: '/recipes', component: RecipesListComponent },
+    { path: '**', redirectTo: 'Not Found' }
+];
