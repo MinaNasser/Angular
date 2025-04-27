8fb34e88583ac603b5d1b22a5aa8a901c3bf6b45
MinaAl5al <minanasser82018@gmail.com>
2025-04-27 16:30:06 +0300

    Add Recipe

--- recipes-app/src/app/app.component.ts
+++ recipes-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { HeaderComponent } from "./header/header.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RecipesListComponent],
+  imports: [RecipesListComponent, AddRecipeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
