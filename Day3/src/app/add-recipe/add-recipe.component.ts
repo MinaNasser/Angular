d33b25fbc829fc9455e9f80fcb7fa6658265a1ba
MinaAl5al <minanasser82018@gmail.com>
2025-04-27 16:56:01 +0300

    Validation

--- Day3/src/app/add-recipe/add-recipe.component.ts
+++ Day3/src/app/add-recipe/add-recipe.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipe',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-recipe.component.html',
   styleUrl: './add-recipe.component.css'
 })
 export class AddRecipeComponent {
+  recipeName: string = '';
+  recipeDescription: string = '';
+
+  HandelSubmit( form :NgForm) {
+    console.log(form);
+    console.log(form.value);
+  }
 
 }
