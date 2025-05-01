2f6faa0e97f8adf6d7e91ca339e9a81c6b4c8f45
MinaAl5al <minanasser82018@gmail.com>
2025-05-01 23:43:19 +0300

    todos

--- To-do/src/app/components/todo-form/todo-form.component.ts
+++ To-do/src/app/components/todo-form/todo-form.component.ts
@@ -1,13 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-form.component.html',
   styleUrl: './todo-form.component.css'
 })
 export class TodoFormComponent {
+  newTodo: string = '';
   @Output() newTodoEvent = new EventEmitter<string>();
 
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.newTodoEvent.emit(this.newTodo.trim());
+      this.newTodo = '';
+    }
+  }
 }
