fcfce4c344d1e1981077652bab33506bad4e46cf
MinaAl5al <minanasser82018@gmail.com>
2025-05-01 04:59:29 +0300

    single

--- firstangularapp/src/app/users/users.component.ts
+++ firstangularapp/src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -35,30 +36,39 @@ export class UsersComponent {
       name: 'John Doe',
       age: 25,
       email: 'WlQkE@example.com',
-      phone: '123-456-7890',  
-      isAdmin : true,
+      phone: '123-456-7890',
+      isAdmin: true,
     },
     {
       name: 'Jane Smith',
       age: 30,
       email: 'jane.smith@example.com',
       phone: '987-654-3210',
-      isAdmin : false,  
+      isAdmin: false,
     },
     {
       name: 'Bob Johnson',
       age: 40,
       email: 'bob.johnson@example.com',
       phone: '555-555-5555',
-      isAdmin : true,
+      isAdmin: true,
     },
     {
       name: 'Alice Brown',
       age: 12,
       email: 'alice.brown@example.com',
       phone: '111-222-3333',
-      isAdmin : false,
+      isAdmin: false,
     }
-    ];
+  ];
+
+  firstName = signal('Morgan');
+  firstNameCapitalized = computed(() => this.firstName().toUpperCase());
+
+  constructor() {
+    console.log(this.firstNameCapitalized()); // MORGAN
+  }
+
+
 
 }
