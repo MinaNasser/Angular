e58dd4743eb7efe704084be957a4de5df3de12d5
MinaAl5al <minanasser82018@gmail.com>
2025-05-02 03:16:12 +0300

    Service AND  DI

--- ECommerceApp/src/app/Components/product/product.component.ts
+++ ECommerceApp/src/app/Components/product/product.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnChanges, Output } from '@angular/core';
 import { iProduct } from '../../Models/iproduct';
 import { CommonModule } from '@angular/common';
 import { iCategory } from '../../Models/icategory';
 import { FormsModule } from '@angular/forms';
 import { HighlightCardDirective } from '../../directives/highlight-card.directive';
 import { SquarePipe } from '../../pipe/square.pipe';
+import { StaticProductService } from '../../services/static-product.service';
 
 @Component({
   selector: 'app-product',
@@ -13,7 +14,8 @@ import { SquarePipe } from '../../pipe/square.pipe';
     CommonModule,
     FormsModule,
     HighlightCardDirective,
-    SquarePipe
+    SquarePipe,
+
   ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
@@ -22,33 +24,21 @@ export class ProductComponent implements OnChanges {
 
   products: iProduct[];
   @Input() recievedCategoryID: number = 0;
-
-
-  // define event
-
-
   FilteredProduct: iProduct[] = [];
   totalOrderPrice: number = 0;
   MyDate: Date = new Date();
   MyNumber: number = 2;
 
+
   @Output() onTotalOrderPriceChanged : EventEmitter<number> ;
 
 
 
-  constructor() {
-
+  // productService=inject(StaticProductService);
+  constructor(private _productService: StaticProductService) {
+    this.products = this._productService.getProducts();
     this.onTotalOrderPriceChanged = new EventEmitter<number>();
-    this.products = [
-      { id: 1, name: 'Laptop', price: 100, quantity: 0, imgUrl: 'https://picsum.photos/200/300', categoryId: 1 },
-      { id: 2, name: 'Laptop Dell', price: 100, quantity: 1, imgUrl: 'https://picsum.photos/200/301', categoryId: 1 },
-      { id: 3, name: 'Phone', price: 200, quantity: 2, imgUrl: 'https://picsum.photos/200/302', categoryId: 2 },
-      { id: 4, name: 'Tablet', price: 300, quantity: 30, imgUrl: 'https://picsum.photos/200/303', categoryId: 2 },
-      { id: 5, name: 'Monitor', price: 400, quantity: 40, imgUrl: 'https://picsum.photos/200/304', categoryId: 4 },
-      { id: 6, name: 'Mouse', price: 500, quantity: 50, imgUrl: 'https://picsum.photos/200/305', categoryId: 4 },
-      { id: 7, name: 'Keyboard', price: 600, quantity: 60, imgUrl: 'https://picsum.photos/200/306', categoryId: 4 },
-      { id: 8, name: 'Headphones', price: 700, quantity: 70, imgUrl: 'https://picsum.photos/200/307', categoryId: 4 },
-    ];
+
 
   }
   Buy(count: string, price: number) {
---
+++ ECommerceApp/src/app/services/static-product.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { iProduct } from '../Models/iproduct';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StaticProductService {
+  private products: iProduct[];
+  constructor() {
+    this.products = [
+      { id: 1, name: 'Laptop', price: 100, quantity: 0, imgUrl: 'https://picsum.photos/200/300', categoryId: 1 },
+      { id: 2, name: 'Laptop Dell', price: 100, quantity: 1, imgUrl: 'https://picsum.photos/200/301', categoryId: 1 },
+      { id: 3, name: 'Phone', price: 200, quantity: 2, imgUrl: 'https://picsum.photos/200/302', categoryId: 2 },
+      { id: 4, name: 'Tablet', price: 300, quantity: 30, imgUrl: 'https://picsum.photos/200/303', categoryId: 2 },
+      { id: 5, name: 'Monitor', price: 400, quantity: 40, imgUrl: 'https://picsum.photos/200/304', categoryId: 4 },
+      { id: 6, name: 'Mouse', price: 500, quantity: 50, imgUrl: 'https://picsum.photos/200/305', categoryId: 4 },
+      { id: 7, name: 'Keyboard', price: 600, quantity: 60, imgUrl: 'https://picsum.photos/200/306', categoryId: 4 },
+      { id: 8, name: 'Headphones', price: 700, quantity: 70, imgUrl: 'https://picsum.photos/200/307', categoryId: 4 },
+    ];
+
+  }
+  getProducts(): iProduct[] {
+    return this.products;
+  }
+  getProduct(id: number): iProduct|undefined {
+    let product = this.products.find(product => product.id == id);
+    return product?product:undefined;
+  }
+  getProductsByCategory(categoryId: number): iProduct[] | undefined {
+    let filteredProducts = this.products.filter(product => product.categoryId == categoryId);
+    return filteredProducts?filteredProducts:undefined;
+  }
+  getProductsByPrice(minPrice: number, maxPrice: number): iProduct[] {
+    return this.products.filter(product => product.price >= minPrice && product.price <= maxPrice);
+  }
+
+}
