0754d393faddf7101d6282f244fef22c497f903e
MinaAl5al <minanasser82018@gmail.com>
2025-05-05 01:30:30 +0300

    Reactive x  & Observable

--- ECommerceApp/src/app/services/Notification.service.ts
+++ ECommerceApp/src/app/services/Notification.service.ts
@@ -1,10 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  notification: string[];
+  constructor() {
+    this.notification = [
+      'Product added to cart',
+      'Product removed from cart',
+      'Product updated',
+      'Product deleted',
+      'Product added to wishlist',
+      'Product removed from wishlist',
+      'Product added to compare',
+      'Product removed from compare',
+      'Product added to compare',
+      'Product removed from compare',
+      'Product added to compare',
+      'Product removed from compare',
+      'Product added to compare',
+      '',
+      'sheared Succcessfully',
+    ];
+  }
 
-  constructor() { }
+  getNotification() {
+    let myObservable = new Observable(observer => {
+      // observer.next(this.notification);
+      // observer.complete();
+      // observer.error('Error');
+
+      let content = 0;
+      setInterval(() => {
+        observer.next(this.notification[content++]);
+        if (content >= this.notification.length) {
+          observer.complete();
+        }
+        if(this.notification[content] == '') {
+          observer.error('Error occured in notification');
+        }
+        // observer.complete();
+      }, 2000);
+    })
+  }
 
 }
