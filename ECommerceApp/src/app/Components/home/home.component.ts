c905bada6a4f54cd16e6669822d7dee963d403b4
MinaAl5al <minanasser82018@gmail.com>
2025-05-05 02:09:01 +0300

    pipe

--- ECommerceApp/src/app/Components/home/home.component.ts
+++ ECommerceApp/src/app/Components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NotificationService } from '../../services/Notification.service';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,39 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit ,OnDestroy{
+
+  notification: string[] = [];
+  subscription!: Subscription ;
+  constructor( private _notificationS: NotificationService){
+
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  ngOnInit(): void {
+    // this._notificationS.getNotification().subscribe((data) => {
+    //   console.log(data);
+    // })
+
+    this.subscription = this._notificationS.getNotification()
+    .pipe(
+      map((data) => data.toUpperCase()),
+      
+      filter((data) => data != '')
+    )
+    .subscribe({
+      next: (data) => {
+        this.notification.push(data);
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('complete');
+      }
+    })
+  }
 
 }
--- ECommerceApp/src/app/services/Notification.service.ts
+++ ECommerceApp/src/app/services/Notification.service.ts
@@ -12,6 +12,7 @@ export class NotificationService {
       'Product removed from cart',
       'Product updated',
       'Product deleted',
+      // '',
       'Product added to wishlist',
       'Product removed from wishlist',
       'Product added to compare',
@@ -21,7 +22,6 @@ export class NotificationService {
       'Product added to compare',
       'Product removed from compare',
       'Product added to compare',
-      '',
       'sheared Succcessfully',
     ];
   }
@@ -33,16 +33,27 @@ export class NotificationService {
       // observer.error('Error');
 
       let content = 0;
-      setInterval(() => {
+     let timer = setInterval(() => {
+      console.log(content);
         observer.next(this.notification[content++]);
-        if (content >= this.notification.length) {
+        if (content == this.notification.length) {
           observer.complete();
+          // content = 0;
+
         }
         if(this.notification[content] == '') {
           observer.error('Error occured in notification');
         }
         // observer.complete();
-      }, 2000);
+      }, 1000);
+
+
+      return {
+        unsubscribe() {
+          console.log('Unsubscribed');
+          clearInterval(timer);
+        }
+      }
     })
   }
 
