8c27620677980765cc77a2f34719121d7de702e4
MinaAl5al <minanasser82018@gmail.com>
2025-05-07 02:47:34 +0300

    APIProductService

--- ECommerceApp/src/app/Gaurds/Auth.guard.ts
+++ ECommerceApp/src/app/Gaurds/Auth.guard.ts
@@ -1,8 +1,20 @@
-import type { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router, type CanActivateFn } from '@angular/router';
+import { UserAuthService } from '../services/userAuth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  let _userAuth = inject(UserAuthService);
+  let router = inject(Router);
 
-  return true   ;
+  if (_userAuth.getUserLoggedIn()) {
+    return true;
+  }
+  else {
+    // alert('Please login first');
+    // window.location.href = '/login';
+    router.navigateByUrl('/login');
+    return false;
+  }
 
 
 };
