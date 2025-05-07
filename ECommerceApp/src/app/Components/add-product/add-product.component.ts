d2034feb059052fe27de8221211987ed4aef8131
MinaAl5al <minanasser82018@gmail.com>
2025-05-07 23:47:49 +0300

    Add Product

---
+++ ECommerceApp/src/app/Components/add-product/add-product.component.ts
@@ -0,0 +1,34 @@
+import { FormsModule } from '@angular/forms';
+import { iCategory } from './../../Models/icategory';
+import { iProduct } from './../../Models/iproduct';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { APICategoryService } from '../../services/apicategory.service';
+
+@Component({
+  selector: 'app-add-product',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './add-product.component.html',
+  styleUrl: './add-product.component.css'
+})
+export class AddProductComponent implements OnInit{
+  categories : iCategory[] = [];
+  product: iProduct ={} as iProduct
+  constructor(private _catSrv:APICategoryService){
+
+  }
+  ngOnInit(){
+    this._catSrv.GetAllCategories().subscribe({
+      next:(res)=>{
+        this.categories = res
+      },
+      error:(err)=>{
+        console.log(err);
+
+      }
+    })
+  }
+
+
+}
