82978f37c5bf58075a7baf3d22bc8faad67ffdfc
MinaAl5al <minanasser82018@gmail.com>
2025-05-07 03:10:06 +0300

    environment

--- ECommerceApp/src/app/services/apiproduct.service.ts
+++ ECommerceApp/src/app/services/apiproduct.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { iProduct } from '../Models/iproduct';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIProductService {
+  // http = inject(HttpClient);
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+  // getProducts  from api
+  GetAllProducts():Observable<iProduct[]>{
+   return this.httpClient.get<iProduct[]>(`${environment.apiUrl}/products`)
+  }
 
-  constructor() { }
+  GetProductById(id:number) : Observable<iProduct>{
+    return this.httpClient.get<iProduct>(`${environment.apiUrl}/products/${id}`)
+  }
+
+  GetProductsByCategory(id:number):Observable<iProduct[]>{
+    return this.httpClient.get<iProduct[]>(`${environment.apiUrl}/products?categoryId=${id}`)
+  }
+  // add product
+  AddProduct(product:iProduct):Observable<iProduct>{
+    return this.httpClient.post<iProduct>(`${environment.apiUrl}/products`,product)
+  }
+  // update product
+  UpdateProduct(product:iProduct):Observable<iProduct>{
+    return this.httpClient.put<iProduct>(`${environment.apiUrl}/products/${product.id}`,product)
+  }
+  // delete product
+  DeleteProduct(id:number):Observable<iProduct>{
+    return this.httpClient.delete<iProduct>(`${environment.apiUrl}/products/${id}`)
+  }
+  
 }
