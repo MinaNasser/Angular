7ca08e45ed9177d9420f9b41b09fd78367380d9f
MinaAl5al <minanasser82018@gmail.com>
2025-05-11 02:28:03 +0300

    headers

--- ECommerceApp/src/app/interceptors/auth-interceptor.service.ts
+++ ECommerceApp/src/app/interceptors/auth-interceptor.service.ts
@@ -3,7 +3,13 @@ import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
 export function authInterceptor(req :HttpRequest<any>,next : HttpHandlerFn){
   // console.log(req);
   // return next(req);
+  let newReq = req;
   if(req.method=="POST"){
-    return next(req);
+    // return next(req);
+     newReq = req.clone({
+      headers:req.headers.append("lang: ","en")
+    })
   }
+  // console.log(newReq);
+  return next(newReq);
 }
