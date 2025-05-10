572cfb8bca8ca06c3f1e4af83cfe5ad58a914262
MinaAl5al <minanasser82018@gmail.com>
2025-05-11 02:10:26 +0300

    req.method=="POST"

--- ECommerceApp/src/app/interceptors/auth-interceptor.service.ts
+++ ECommerceApp/src/app/interceptors/auth-interceptor.service.ts
@@ -1,6 +1,9 @@
 import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
 
 export function authInterceptor(req :HttpRequest<any>,next : HttpHandlerFn){
-  console.log(req);
-  return next(req);
+  // console.log(req);
+  // return next(req);
+  if(req.method=="POST"){
+    return next(req);
+  }
 }
