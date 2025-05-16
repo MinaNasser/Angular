46465d630731d4748e951972e02ebf0b2c1f739f
MinaAl5al <minanasser82018@gmail.com>
2025-05-16 15:28:57 +0300

    phoneNumbers

--- ECommerceApp/src/app/Components/register/register.component.ts
+++ ECommerceApp/src/app/Components/register/register.component.ts
@@ -1,11 +1,11 @@
 
-import { JsonPipe } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, NgFor],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -44,13 +44,24 @@ export class RegisterComponent implements OnInit {
           Validators.required,
           Validators.minLength(3)
         ])
-      })
+      }),
+      phoneNumbers: new FormArray(
+        [
+          new FormControl('', [
+            Validators.required,
+            Validators.minLength(11),
+            Validators.maxLength(11),
+            Validators.pattern('[0-9]+')
+          ])
+        ]
+      )
+
     }, { validators: this.matchPasswordsValidator });
 
 
   }
   ngOnInit(): void {
-    this.userRegisterForm.setValue({
+    this.userRegisterForm.patchValue({
       name: 'Ahmed',
       email: 'ahmed@gmail.com',
       password: 'Asd@2030asd',
@@ -65,33 +76,53 @@ export class RegisterComponent implements OnInit {
     alert("User Added Successfully");
     console.log(this.userRegisterForm.value);
   }
-  get name(){
+  get name() {
     return this.userRegisterForm.get('name');
   }
- get email() {
-    return this.userRegisterForm.get('email') ;
+  get email() {
+    return this.userRegisterForm.get('email');
   }
   get password() {
-    return this.userRegisterForm.get('password') ;
+    return this.userRegisterForm.get('password');
   }
   get confirmPassword() {
-    return this.userRegisterForm.get('confirmPassword') ;
+    return this.userRegisterForm.get('confirmPassword');
   }
 
   get city() {
-    return this.userRegisterForm.get('Address.city') ;
+    return this.userRegisterForm.get('Address.city');
   }
   get street() {
     return this.userRegisterForm.get('Address.street');
   }
+  get phoneNumbers() {
+    return this.userRegisterForm.get('phoneNumbers') as FormArray;
+  }
 
 
   matchPasswordsValidator(group: AbstractControl): ValidationErrors | null {
-  const password = group.get('password')?.value;
-  const confirmPassword = group.get('confirmPassword')?.value;
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
 
-  return password === confirmPassword ? null : { passwordMismatch: true };
-}
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+  addPhoneNumber() {
+    const phoneNumbers = this.phoneNumbers;
+    phoneNumbers.push(new FormControl('', [
+      Validators.required,
+      Validators.minLength(11),
+      Validators.maxLength(11),
+      Validators.pattern('[0-9]+')
+    ]));
+  }
+  removePhoneNumber(index: number) {
 
+    const phoneNumbers = this.phoneNumbers;
+    if (phoneNumbers.length > 1) {
+      phoneNumbers.removeAt(index);
+    }
+
+    // phoneNumbers.removeAt(index);
+  }
 
 }
